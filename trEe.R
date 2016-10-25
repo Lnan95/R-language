8401929d59970d63111e6badb77dec1546fc1393
Lnan95 <464818805@qq.com>
2016-10-25 14:29:57 +0800

    tRee1.3
    improve the effeciency of the function

--- trEe.R
+++ trEe.R
@@ -1,69 +1,129 @@
-tree = function(train,label,w=100) {
-  mi = min(train)
-  ma = max(train)
-  box = seq(mi,ma,length.out = w)
-  gini = as.numeric()
+box<-function(dot,x,y)
+{
   
-  for (i in box) {
-    
-    corr_1 = (train>mi) & (train<=i)
-    cont_1 = as.character(label[corr_1])
-    corr_2 = (train<=ma) & (train>i)
-    cont_2 = as.character(label[corr_2])
-    p1 = sum(cont_1 == "setosa")/length(cont_1)
-    p2 = sum(cont_1 == "virginica")/length(cont_1)
-    p3 = sum(cont_1 == "versicolor")/length(cont_1)
-    
-    p1_2 = sum(cont_2 == "setosa")/length(cont_2)
-    p2_2 = sum(cont_2 == "virginica")/length(cont_2)
-    p3_2 = sum(cont_2 == "versicolor")/length(cont_2)
-    
-    gini =c(gini, p1*p2*p3+p1_2*p2_2*p3_2)
-  }
+  x>dot
+  x<=dot
+  l1=  label[x>dot]
+  l2 = label[x<=dot]
+  p1 = sum(l1 == "setosa")/length(l1)
+  p2 = sum(l1 == "virginica")/length(l1)
+  p3 = sum(l1 == "versicolor")/length(l1)
+  
+  p1_2 = sum(l2 == "setosa")/length(l2)
+  p2_2 = sum(l2 == "virginica")/length(l2)
+  p3_2 = sum(l2 == "versicolor")/length(l2)
+  gini = p1*p2*p3+p1_2*p2_2*p3_2
+  return(gini)
+}
+
+
+
+divide = function(train,label,w=100,box=box)
+{
+  train = train[order(train)]
+  gini = sapply(xx,box,x=train,y=label)
+  
   which_mini_gini = which.min(gini)
-  bound = box[which_mini_gini]
+  bound = train[which_mini_gini]
   
-  if (gini[which_mini_gini]==0)
-  {
-    sep=train-bound
-    sep_1 = max(sep[sep<0])
-    sep_2 = min(sep[sep>0])
-    bound = (sep_2-sep_1)/2 + bound
-  }
-  
+  sep=train-bound
+  sep_2 = min(sep[sep>0])
+  bound = sep_2/2 + bound
   
   return(data.frame(bound=bound,min_gini=gini[which_mini_gini]))
 }
 
-#> tree(x$Petal.Length,label)
-#bound min_gini
-#1 2.503535        0
-for (i in c(1:(w-which.min(gini)))) 
+
+
+tree <- function(x,label,fun=divide,is_random=FALSE,is_bagging=FALSE)
 {
-  if (sum((train>box[which.min(gini)]) & (train<box[which.min(gini)+i])>0))
+  p = dim(x)[2L]
+  n = dim(x)[1L]
+  
+  if (is_random)
   {
-    break
+    
+    l=sample(x=c(1:n),size=round(n*2/3),replace = TRUE)
+    x = x[l,]
+    label = label[l]
+    m = round(sqrt(p))
+    feature <- sample(x = c(1:p),size=m)
+    x = x[,feature]
+    p = m
+    
+    div = as.data.frame(apply(x,2,fun,label=label))
+    div_mini_gini = div[seq(2,to =p*2,by = 2)]
+    div_bound = div[seq(1,to = p*2,by = 2)]
+    whic = which.min(div_mini_gini)
+    frame = data.frame(bound=div_bound[whic],which=feature[whic],row.names = NULL)
+    
+    return(frame)
   }
-  bound = 1/2*i*(ma-mi)/w+box[which.min(gini)]
-}
-min_gini=min(gini[!is.na(gini)])
-#> tree(x$Petal.Length,label)
-#bound min_gini
-#1 2.455035        0
-
-
-tree_2 <- function(x,label,fun=tree)
-{
-  n = dim(x)[2L]
-  div = as.data.frame(apply(x,2,tree,label=label))
-  div_mini_gini = div[seq(2,to =n*2,by = 2)]
-  div_bound = div[seq(1,to = n*2,by = 2)]
+  
+  if (is_bagging)
+  {
+    l=sample(x=c(1:n),size=round(n*2/3),replace = TRUE)
+    x = x[l,]
+    label = label[l]
+  }
+  
+  div = as.data.frame(apply(x,2,fun,label=label))
+  div_mini_gini = div[seq(2,to =p*2,by = 2)]
+  div_bound = div[seq(1,to = p*2,by = 2)]
   whic = which.min(div_mini_gini)
   frame = data.frame(bound=div_bound[whic],which=whic,row.names = NULL)
   
   return(frame)
 }
 
+#first bagging tree
+
+frame = data.frame(bound=0,which=0)
+for (i in c(1:1000)) 
+{
+  new=tree(x,label,is_bagging = TRUE)
+  names(new)<-c("bound","which")
+  frame = rbind(frame,new)
+}
+
+frame = frame[-1,]
+one = sum(frame$which==1)
+two = sum(frame$which==2)
+three = sum(frame$which==3)
+four = sum(frame$which==4)
+
+
+which.div = which.max(c(one,two,three,four))
+mean(frame[frame$which==which.div,1])
 
 
 
+#first random forest 
+
+frame = data.frame(bound=0,which=0)
+for (i in c(1:1000)) 
+{
+  new=tree(x,label,is_random = TRUE)
+  names(new)<-c("bound","which")
+  frame = rbind(frame,new)
+}
+
+frame = frame[-1,]
+one = sum(frame$which==1)
+two = sum(frame$which==2)
+three = sum(frame$which==3)
+four = sum(frame$which==4)
+
+
+which.div = which.max(c(one,two,three,four))
+mean(frame[frame$which==which.div,1])
+
+
+#boosting#Î´×÷
+
+
+
+#
+
+
+  
